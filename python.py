7a28aa71a40ab3cf2a7d9607345e4d334661fa9d
JosephMusya <josephmusya254@gmail.com>
2022-08-07 13:23:05 +0300

    Working

--- python.py
+++ python.py
@@ -1,8 +1,9 @@
 import serial
 import time
+import sys
 #com_port = '/dev/ttyACM0'
 com_port = 'COM18'
-baudrate = 9600
+baudrate = 4800
 if __name__ == '__main__':
     try:
         ser = serial.Serial(com_port,baudrate,timeout=5)
@@ -10,13 +11,20 @@ if __name__ == '__main__':
         print("Connected to com port ".format(com_port))
     except:
         print("Error opening the Port!")
+        sys.exit()
     while True:
-        i = input("CMD ").strip()
+        i = input("CMD: ").strip()
         if i.lower() == 'kill':
             print("Exit...")
             ser.close()
-            break
-        ser.write(i.encode())
-        time.sleep(.05)
-        feedback = ser.readline().decode('ascii')
-    
+            sys.exit()
+        else:
+            cmd = i+'\r'
+            print("Sending data...")
+            ser.write(cmd.encode())
+        # ser.write(b'on')
+        # time.sleep(1)
+        # ser.write(b'off')
+        # time.sleep(1)
+        # feedback = ser.read().decode('utf-8')
+        # print(feedback)
