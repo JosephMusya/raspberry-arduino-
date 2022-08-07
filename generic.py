ba4924bac7471dfee9edecf7004c5f0bb3b32f15
JosephMusya <josephmusya254@gmail.com>
2022-08-07 21:01:48 +0300

    Async communication

--- generic.py
+++ generic.py
@@ -15,29 +15,27 @@ def connect(com_port,baudrate):
         print(f"Connected to {com_port}")
         return ser
     except:
-        print(f"Error Opening the {com_port}"),
+        print(f"Error Opening {com_port}"),
         print(" Check whether the Port is busy or you entered the correct port number")
 async def readArduino():
     return ser.read().decode()
 
 async  def writeArduino():
-    i = input("CMD: ")
-    cmd = i+'\r'
-    ser.write(cmd.encode())
+    cmd = input("CMD1: ")
+    cmd = cmd+'\r'   
     
 ser = connect(com_port,baudrate)
+
 async def main():
     while True:
         val = r.randint(0,10)
-        print(val)
         if val >= 7:
-            task1 = asyncio.create_task(writeArduino())        
-            task2 = asyncio.create_task(readArduino()) 
-            
+            await asyncio.create_task(writeArduino())        
+            task2 = asyncio.create_task(readArduino())             
             feedback = await task2        
             print(feedback)      
             print("Completed task...")
-            await asyncio.sleep(1)
+            await asyncio.sleep(0.5)        
 
 if __name__ == '__main__':
     asyncio.run(main())
