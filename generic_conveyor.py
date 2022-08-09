e10538e505582434b3d817c883210c67a4b489d8
JosephMusya <josephmusya254@gmail.com>
2022-08-09 23:01:55 +0300

    Modified

--- generic_conveyor.py
+++ generic_conveyor.py
@@ -3,9 +3,7 @@ import asyncio
 import time
 import sys
 
-import random as r
 #com_port = '/dev/ttyACM0'
-com_port = 'COM18'
 baudrate = 4800
 print("System Initializing...")
 time.sleep(3)
@@ -36,24 +34,46 @@ async def pick():
     for i in range(0,5):
         print("Picking...")
         await asyncio.sleep(1)
+                
+async def main():   
+    ''' 
+    usage:
+        >>> if ser: asyncio.run(main())
         
-serial_comm = lambda ports: connect(ports)
-ser = serial_comm(serial.tools.list_ports.comports())
-
-async def main():    
-    while True:
+        This is the main function:
+        Starts by connecting to the conveyor by sending the datapacket=ONLINE
+        if there was a connection
+        If the connection is open, python reads the data coming from the conveyor
+        When python gets a 1, it interprets that there was an object on the target 
+        pos of the conveyor and initiates the sorting process
+        
+        During this time, the conveyor status is 1 and is only triggered to send
+        another sorting action when the status changes from a 1 to 0
+        
+        Incase an error occurs in the python, an OFFLINE command is sent to the conveyor
+        which stops the conveyor
+    ''' 
+    
+    if ser.isOpen(): await asyncio.create_task(writeArduino(data='ONLINE'))    
+    while True:  
         try:
-            await asyncio.create_task(writeArduino(data='ONLINE'))
-            res = await asyncio.create_task(readArduino())
-            print(res)
-        
-            if res == '1':
-                await asyncio.create_task(pick())
-        except:
-            await asyncio.create_task(writeArduino(data='OFFLINE'))
-            ser.close()
-            
+            if ser.isOpen():
+                try:
+                    res = await asyncio.create_task(readArduino())
+                    print(f"From Conveyor => {res}")
+                
+                    if res == '1':
+                        await asyncio.create_task(pick())  
+                except:
+                    await asyncio.create_task(writeArduino('OFFLINE'))
+                    ser.close()
+        except Exception:
+            print("Broken Communication to the conveyor")
+            sys.exit()
 if __name__ == '__main__':
     print("Starting...")
-    time.sleep(2)
-    asyncio.run(main())
+    serial_comm = lambda ports: connect(ports)
+    ser = serial_comm(serial.tools.list_ports.comports())   
+    time.sleep(3)
+    if ser: asyncio.run(main())
+    print("Not communication to the identified ports!")
