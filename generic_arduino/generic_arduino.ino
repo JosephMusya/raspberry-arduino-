f3f8c126fd22e7c55ef5ddc21928a01b0f793fc7
JosephMusya <josephmusya254@gmail.com>
2022-08-11 17:42:14 +0300

    Added the distance measurement

--- generic_arduino/generic_arduino.ino
+++ generic_arduino/generic_arduino.ino
@@ -12,41 +12,37 @@ String data;
 
 bool system_status = false;
 bool sent = false;
-
 void setup() {
   // put your setup code here, to run once:
   pinMode(led, OUTPUT);
   pinMode(transmitter, OUTPUT);
+
 
   Serial.begin(9600);
 }
 
 void loop() {
   if (Serial.available() > 0) {
-    getData();
+    data = Serial.readStringUntil('\r');
+    if (data == "ONLINE") {
+      system_status = true;
+    }
+
+    else if (data == "OFFLINE") {
+      system_status = false;
+    }
   }
 
   if (system_status) {
     start();
-    delay(10);
   }
   else {
     alert();
   }
 
+  //Serial.println(random_val);
 
 }
-void getData() {
-  data = Serial.readStringUntil('\r');
-  if (data == "ONLINE") {
-    system_status = true;
-  }
-
-  else if (data == "OFFLINE") {
-    system_status = false;
-  }
-}
-
 void alert() {
   digitalWrite(led, HIGH);
   delay(50);
@@ -55,29 +51,39 @@ void alert() {
 }
 void start() {
   digitalWrite(transmitter, HIGH);
-  delayMicroseconds(10);
-  received = analogRead(A0);
-  //  Serial.println(received);
-  if (received < 600) {
-    sensor_status = 1;
-    stopConveyor();    
+  delayMicroseconds(2);
+  received = 1023.0 - analogRead(A0);
 
+  digitalWrite(transmitter, LOW);
+  delayMicroseconds(2);
+  noise = 1023.0 - analogRead(A0);
+
+  denoised = received - noise;
+  //  Serial.println(denoised);
+
+  if (denoised > 200) {
+    sensor_status = 1;
+    stopConveyor();
     if (!sent) {
       Serial.print(sensor_status);
       digitalWrite(led, HIGH);
       sent = true;
-      while (true){
-        String progress = Serial.readStringUntil('\r');        
-        if (progress == "DONE"){
-          break;
+
+      while (true) {
+        if (Serial.available() > 0) {
+          String progress =  Serial.readStringUntil('\r');
+          if ((progress) == "DONE") {
+            break;
+          }
         }
       }
-    }    
+    }
+
   }
-  else if (received > 600) {
+  else {
     moveConveyor();
-    sent = false;
     digitalWrite(led, LOW);
+    sent = false;
   }
 }
 
